99dbe3815ecd0851e0a9ea4ac2e2b8950ba00328
caceresdaniel <mattmoocow@live.com>
2017-10-29 20:29:22 -0700

    connected charactercatalogcontroller servlet with the charactercatalog jsp

--- src/lab6/SimpsonsCharacter.java
+++ src/lab6/SimpsonsCharacter.java
@@ -1,5 +1,31 @@
 package lab6;
 
 public class SimpsonsCharacter {
+	
+	String name;
+	int numOfPhotos;
+	
+	public SimpsonsCharacter(String name, int numOfPhotos){
+		this.name = name;
+		this.numOfPhotos = numOfPhotos;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumOfPhotos() {
+		return numOfPhotos;
+	}
+
+	public void setNumOfPhotos(int numOfPhotos) {
+		this.numOfPhotos = numOfPhotos;
+	}
+	
+	
 
 }
